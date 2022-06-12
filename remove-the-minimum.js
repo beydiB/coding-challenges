5503ac4a0bb22399fbd17ba5739bd36cd27f3290
Beidi <beidi.beer@gmail.com>
2022-06-12 21:28:41 +0000

    edit

--- remove-the-minimum.js
+++ remove-the-minimum.js
@@ -1,6 +1,6 @@
 //Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
 //Don't change the order of the elements that are left.
-
+//ex: Input: [2,2,1,2,1], output = [2,2,2,1]
 function removeSmallest(numbers) {
     let slicedArr = numbers.slice()
     let lowR = numbers.indexOf(Math.min(...numbers))
