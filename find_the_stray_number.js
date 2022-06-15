f1cbbb52fbf13f53f59b14e39a727a6d141329b6
Beidi <beidi.beer@gmail.com>
2022-06-15 19:27:28 +0000

    first commit

---
+++ find_the_stray_number.js
@@ -0,0 +1,4 @@
+//You are given an odd-length array of integers, in which all of them are the same, except for one single number. Complete the method which accepts such an array, and returns that single different number. The input array will always be valid! (odd-length >= 3)
+//[1, 1, 2] ==> 2
+//[17, 17, 3, 17, 17, 17, 17] ==> 3
+const stray = (numbers) => numbers.filter(el => numbers.indexOf(el) === numbers.lastIndexOf(el))[0]
