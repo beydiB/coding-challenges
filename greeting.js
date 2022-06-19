692cb6e09b541a0ac2944e18b995bc312305f07f
Beidi <beidi.beer@gmail.com>
2022-06-19 14:21:17 +0000

    Create a function that gives a personalized greeting

---
+++ greeting.js
@@ -0,0 +1,6 @@
+//Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+//name equals owner return	'Hello boss'. otherwise return	'Hello guest'
+
+function greet (name, owner) {
+    return name === owner? 'Hello boss':'Hello guest'
+    }
