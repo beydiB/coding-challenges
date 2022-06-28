22e9698660ce1dd9ae39dbb347279c936c5fbf9f
Beidi <beidi.beer@gmail.com>
2022-06-28 22:26:06 +0000

    returns the sum of the absolute value of each of the number's decimal digits.

---
+++ summing_numbers_digits.js
@@ -0,0 +1,8 @@
+//Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+//10 --> 1
+//99 --> 18
+//-32 --> 5
+
+function sumDigits(number) {
+    return Math.abs(number).toString().split('').reduce(function(a,b){return +a + +b}, 0);
+  }
