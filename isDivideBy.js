1777ad916e906c46732ee0d47ba1efd0a1cdfc45
Beidi <beidi.beer@gmail.com>
2022-07-01 23:04:30 +0000

    check if an integer number is divisible by both integers a and b

---
+++ isDivideBy.js
@@ -0,0 +1,6 @@
+//Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.t
+
+function isDivideBy(number, a, b) {
+    return number % a === 0 && number % b === 0
+      
+    }
