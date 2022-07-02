04400dea9d067292dbcefb0ed2994b2ed392171d
Beidi <beidi.beer@gmail.com>
2022-07-02 22:26:58 +0000

    rock, paper, scissors

---
+++ rps.js
@@ -0,0 +1,14 @@
+//Rock Paper Scissors
+//Let's play! You have to return which player won! In case of a draw return Draw!.
+
+
+const rps = (p1, p2) => {
+    if (p1 === p2) return "Draw!";
+    var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+    if (p2 === rules[p1]) {
+      return "Player 1 won!";
+    }
+    else {
+      return "Player 2 won!";
+    }
+  };
