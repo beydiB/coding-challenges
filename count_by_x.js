691f2cb757001b50f2d8cbedeccdec011ff27f7f
Beidi <beidi.beer@gmail.com>
2022-07-03 23:01:18 +0000

    Create a function with two arguments that will return an array of the first (n) multiples of (x).

---
+++ count_by_x.js
@@ -0,0 +1,12 @@
+//Create a function with two arguments that will return an array of the first (n) multiples of (x).
+//Assume both the given number and the number of times to count will be positive numbers greater than 0. 
+//Return the results as an array
+
+
+function countBy(x, n) {
+    var z = [];
+    for (i = 1; i <= n; i++) {
+        z.push(x * i);
+    }
+    return z;
+}
