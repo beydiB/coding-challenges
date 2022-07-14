bf0a2b2898fbb49648ed7f295efac1b3206cedc5
Beidi <beidi.beer@gmail.com>
2022-07-14 17:49:37 +0000

    Find the capitals

---
+++ find_the_capitals.js
@@ -0,0 +1,10 @@
+//Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+var capitals = function (word) {
+
+let arr = []
+word.split('').forEach((x,i) => {
+  if(x == x.toUpperCase()){arr.push(i)}
+})
+return arr
+}
