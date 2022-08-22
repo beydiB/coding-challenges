56fd24c6e29c73879a5418a5b1c6d70167dc1106
Beidi <beidi.beer@gmail.com>
2022-08-22 22:49:52 +0000

    Convert string to camel case

---
+++ string_toCamelCase.js
@@ -0,0 +1,15 @@
+/*
+Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+Examples
+
+"the-stealth-warrior" gets converted to "theStealthWarrior"
+"The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+*/
+
+
+function toCamelCase(str){
+      var regExp=/[-_]\w/ig;
+      return str.replace(regExp,function(match){
+            return match.charAt(1).toUpperCase();
+       });
+}
