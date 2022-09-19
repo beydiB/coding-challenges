98b11ddf39bf4bb70b2c7223416f35e0006db320
Beidi <beidi.beer@gmail.com>
2022-09-19 13:45:06 +0000

    dot Calculator

---
+++ dot_calculator.js
@@ -0,0 +1,20 @@
+/*
+You'll have to return a string that contains dots, as many the equation returns. If the result is 0, return the empty string. When it comes to subtraction, the first number will always be greater than or equal to the second number.
+Examples (Input => Output)
+
+* "..... + ..............." => "...................."
+* "..... - ..."             => ".."
+* "..... - ."               => "...."
+* "..... * ..."             => "..............."
+* "..... * .."              => ".........."
+* "..... // .."             => ".."
+* "..... // ."              => "....."
+* ". // .."                 => ""
+* ".. - .."                 => ""
+*/
+
+function dotCalculator (equation) {
+  equation = equation.replace('//','/')
+  equation = eval(equation.split(' ').map((x,i) => i != 1 ? x.length : x).join(''))
+  return '.'.repeat(equation)
+}
