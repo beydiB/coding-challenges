5334d323ef23fbf19819cce5fe57a71eaf56baf4
Beidi <beidi.beer@gmail.com>
2022-10-02 21:30:11 +0000

    Reverse every other word in the string

---
+++ reverseOtherWord.js
@@ -0,0 +1,8 @@
+/*
+Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+*/
+
+function reverse(str){
+ str = str.split(' ')
+  return str.map((x,i) => i % 2 ? x.split('').reverse().join('') : x ).join(' ')
+}
