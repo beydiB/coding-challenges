b961d9fef0da7615747e89cd560cc4122ff7e378
Beidi <beidi.beer@gmail.com>
2022-10-17 13:07:58 +0000

    Number Of Occurrences

---
+++ numberOccurences.js
@@ -0,0 +1,15 @@
+/*
+Write a function that returns the number of occurrences of an element in an array.
+Examples
+
+var arr = [0, 1, 2, 2, 3];
+arr.numberOfOccurrences(0) === 1;
+arr.numberOfOccurrences(4) === 0;
+arr.numberOfOccurrences(2) === 2;
+arr.numberOfOccurrences("a") === 0;
+*/
+
+
+Array.prototype.numberOfOccurrences = function(n) {
+  return this.filter( x=> x === n ).length
+}
