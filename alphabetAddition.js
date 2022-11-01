0396b10fd55c35f5161d5916960440f582aebf7e
beydiB <98928117+beydiB@users.noreply.github.com>
2022-11-01 11:34:05 +0000

    Alphabet addition

---
+++ alphabetAddition.js
@@ -0,0 +1,23 @@
+/*
+Your task is to add up letters to one letter.
+
+The function will be given a variable amount of arguments, each one being a letter to add.
+Notes:
+
+    Letters will always be lowercase.
+    Letters can overflow (see second to last example of the description)
+    If no letters are given, the function should return 'z'
+
+Examples:
+
+addLetters('a', 'b', 'c') = 'f'
+addLetters('a', 'b') = 'c'
+addLetters('z') = 'z'
+addLetters('z', 'a') = 'a'
+addLetters('y', 'c', 'b') = 'd' // notice the letters overflowing
+addLetters() = 'z'
+*/
+
+function addLetters(...letters) {
+    return String.fromCharCode((letters.reduce( (a,b) => a+b.charCodeAt(0)-96, 0)+25)%26+97);
+}
