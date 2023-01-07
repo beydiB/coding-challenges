9b0a5c7ad1370a856cbb698d52eb9c164ce923c6
beydiB <98928117+beydiB@users.noreply.github.com>
2023-01-07 22:54:46 +0000

    count consonants

---
+++ countConsonants.js
@@ -0,0 +1,7 @@
+/*
+Write a function consonantCount, consonant_count or ConsonantCount that takes a string of English-language text and returns the number of consonants in the string.
+
+Consonants are all letters used to write English excluding the vowels a, e, i, o, u.
+*/
+
+const consonantCount = str => str.replace(/[\W\daeiou_]+/gi, ``).length;
