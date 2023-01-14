83d5c42696e96c8d96588a60f24bf94dbf892b79
unknown <beidi.beer@gmail.com>
2023-01-14 14:32:19 +0000

    Disarium Number (Special Numbers Series #3)

---
+++ disariumNumber.js
@@ -0,0 +1,16 @@
+/*
+Definition
+Disarium number is the number that The sum of its digits powered with their respective positions is equal to the number itself.
+
+Task
+Given a number, Find if it is Disarium or not .
+*/
+
+function disariumNumber(n){
+    n = String(n)
+    let sum = 0
+    for(i = 0; i < n.length; i++) {
+      sum += n[i] ** (i+1)
+    }
+    return sum == n ? "Disarium !!" : "Not !!"
+  }
