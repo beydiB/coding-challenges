9b6f410dfdc2cc9281fcd55f67cabbedbcb7bb2b
unknown <beidi.beer@gmail.com>
2023-02-13 22:51:22 +0000

    Hamming Distance

---
+++ hammingDistance.js
@@ -0,0 +1,25 @@
+/*
+The Hamming Distance is a measure of similarity between two strings of equal length. Complete the function so that it returns the number of positions where the input strings do not match.
+
+Examples:
+a = "I like turtles"
+b = "I like turkeys"
+Result: 3
+
+a = "Hello World"
+b = "Hello World"
+Result: 0
+
+a = "espresso"
+b = "Expresso"
+Result: 2
+You can assume that the two inputs are ASCII strings of equal length.
+*/
+
+function hamming(a,b) {
+	let result = 0
+  for(i = 0; i < a.length; i++) {
+    if(a[i] != b[i]) result ++
+  }
+  return result
+}
