a8996ef48694527b18f74fecd6c58bb4823c177a
unknown <beidi.beer@gmail.com>
2023-03-20 22:14:00 +0000

    If you can read this...

---
+++ ifYouCanReadThis.js
@@ -0,0 +1,26 @@
+/*
+ask
+
+You'll have to translate a string to Pilot's alphabet (NATO phonetic alphabet).
+
+Input:
+
+If, you can read?
+
+Output:
+
+India Foxtrot , Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta ?
+
+Note:
+
+    There are preloaded dictionary you can use, named NATO
+    The set of used punctuation is ,.!?.
+    Punctuation should be kept in your return string, but spaces should not.
+    Xray should not have a dash within.
+    Every word and punctuation mark should be seperated by a space ' '.
+    There should be no trailing whitespace
+*/
+
+function to_nato(words) {
+	return words.toLowerCase().split('').map(x => NATO[x] || x).join(' ').replace(/\s+/g, ' ').trim()
+}
