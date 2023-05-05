61558c40958ac5e4338d96fc854563b9add247f5
unknown <beidi.beer@gmail.com>
2023-05-05 21:29:24 +0000

    The Office III - Broken Photocopier

---
+++ brokenPhotocopier.js
@@ -0,0 +1,13 @@
+/*
+The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+
+Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+
+Given a string of binary, return the version the photocopier gives you as a string.
+*/
+
+function broken(x) {
+  return x.replace(/[01]/g, (x) => {
+    return x == "0" ? "1" : "0";
+  });
+}
