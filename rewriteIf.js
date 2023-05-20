c489767dcb22846efb9f117fe99377d6fe69345b
unknown <beidi.beer@gmail.com>
2023-05-20 21:50:21 +0000

    rewrite 'if' with '?'

---
+++ rewriteIf.js
@@ -0,0 +1,19 @@
+/*
+Rewrite 'if' into '?'
+importance: 5
+Rewrite this if using the ternary operator '?' :
+To solution
+Rewrite 'if..else' into '?'
+importance: 5
+Rewrite if..else using multiple ternary operators '?' .
+For readability, it’s recommended to split the code into multiple lines.
+
+
+if (a + b < 4) {
+result = 'Below';
+} else {
+result = 'Over';
+}
+*/
+
+result = (a + b < 4) ? 'Below' : 'Over'
