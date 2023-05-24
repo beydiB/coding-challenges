470ccbc3e67b41228140a88940cb1fb7ba001a8f
unknown <beidi.beer@gmail.com>
2023-05-24 23:48:06 +0000

    347. Top K Frequent Elements

---
+++ topKFrequentElements.js
@@ -0,0 +1,38 @@
+/*
+347. Top K Frequent Elements
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+ 
+
+Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+*/
+
+
+var topKFrequent = function(nums, k) {
+    let map = {}
+    for(i=0; i < nums.length; i++){
+      if(!map[nums[i]]) map[nums[i]] = 1
+      else{map[nums[i]] ++}
+    }
+    const sortedData = Object.entries(map).sort((a, b) => b[1] - a[1]);
+    return sortedData.slice(0, k).map(([key]) => key);
+  };
+  
