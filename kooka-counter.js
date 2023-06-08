a237bb667f5dd9b372fb13911e7462881f0a41e1
unknown <beidi.beer@gmail.com>
2023-06-08 23:43:34 +0000

    Kooka-Counter

---
+++ kooka-counter.js
@@ -0,0 +1,38 @@
+/*
+A family of kookaburras are in my backyard.
+
+I can't see them all, but I can hear them!
+
+How many kookaburras are there?
+
+Hint
+The trick to counting kookaburras is to listen carefully
+
+The males sound like HaHaHa...
+
+The females sound like hahaha...
+
+And they always alternate male/female
+
+Examples
+ha = female => 1
+Ha = male => 1
+Haha = male + female => 2
+haHa = female + male => 2
+hahahahaha = female => 1
+hahahahahaHaHaHa = female + male => 2
+HaHaHahahaHaHa = male + female + male => 3
+*/
+
+var kookaCounter = function(laughing) {
+if(!laughing) return 0
+let counter = 1
+let currentLetter = laughing[0]
+for(i=0; i < laughing.length; i+=2){
+  if(laughing[i] != currentLetter){
+    counter ++
+    currentLetter = laughing[i]
+  }
+}
+  return counter
+}
