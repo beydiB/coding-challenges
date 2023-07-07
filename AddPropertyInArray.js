32cb6698d797c3af5f1998b72c906c96e472c046
beydiB <98928117+beydiB@users.noreply.github.com>
2023-07-07 23:43:33 +0000

    Update AddPropertyInArray.js

--- AddPropertyInArray.js
+++ AddPropertyInArray.js
@@ -2,7 +2,6 @@
 Your task is to add a new property usersAnswer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length.
 
 For example:
-
 var questions = [{
     question: "What's the currency of the USA?",
     choices: ["US dollar", "Ruble", "Horses", "Gold"],
