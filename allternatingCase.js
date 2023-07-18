9daafb9532f40f2905b787c24dac3055557f2820
beydiB <98928117+beydiB@users.noreply.github.com>
2023-07-18 23:51:11 +0000

    Update allternatingCase.js

--- allternatingCase.js
+++ allternatingCase.js
@@ -1,6 +1,5 @@
 /*
 altERnaTIng cAsE <=> ALTerNAtiNG CaSe
-
 Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
 
 "hello world".toAlternatingCase() === "HELLO WORLD"
