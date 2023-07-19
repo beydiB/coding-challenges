1506582e5f6a61e4ea80adaba98d2943305cc5eb
beydiB <98928117+beydiB@users.noreply.github.com>
2023-07-19 23:40:43 +0000

    Update balanced_number.js

--- balanced_number.js
+++ balanced_number.js
@@ -2,7 +2,6 @@
 A balanced number is a number where the sum of digits to the left of the middle digit(s) and the sum of digits to the right of the middle digit(s) are equal.
 
 If the number has an odd number of digits, then there is only one middle digit. (For example, 92645 has one middle digit, 6.) Otherwise, there are two middle digits. (For example, the middle digits of 1301 are 3 and 0.)
-
 The middle digit(s) should not be considered when determining whether a number is balanced or not, e.g. 413023 is a balanced number because the left sum and right sum are both 5.
 The task
 
