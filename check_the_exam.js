dfec3adba43201fb6a6805eedc44e4f7b070f9db
beydiB <98928117+beydiB@users.noreply.github.com>
2023-07-23 23:51:06 +0000

    Update check_the_exam.js

--- check_the_exam.js
+++ check_the_exam.js
@@ -4,7 +4,6 @@ The first input array is the key to the correct answers to an exam, like ["a", "
 The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
 
 If the score < 0, return 0.
-
 For example:
 
 checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
