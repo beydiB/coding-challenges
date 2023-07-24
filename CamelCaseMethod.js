ad6533c33913d310b8fd06056ee1fd4736847c1d
unknown <beidi.beer@gmail.com>
2023-07-24 23:10:27 +0000

    CamelCase Method

---
+++ CamelCaseMethod.js
@@ -0,0 +1,15 @@
+/*
+Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+
+For instance:
+
+"hello case".camelCase() => HelloCase
+"camel case word".camelCase() => CamelCaseWord
+
+Don't forget to rate this kata! Thanks :)
+*/
+
+String.prototype.camelCase=function(){
+    if(!this.length) return ""
+    return this.split(' ').map(x => x[0].toUpperCase()+ x.slice(1)).join('')
+  }
