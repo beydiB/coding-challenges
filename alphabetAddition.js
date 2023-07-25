1af9c7c24bd1db4d307dc175e7ea2762c803165f
beydiB <98928117+beydiB@users.noreply.github.com>
2023-07-25 23:49:49 +0000

    Update alphabetAddition.js

--- alphabetAddition.js
+++ alphabetAddition.js
@@ -3,7 +3,6 @@ Your task is to add up letters to one letter.
 
 The function will be given a variable amount of arguments, each one being a letter to add.
 Notes:
-
     Letters will always be lowercase.
     Letters can overflow (see second to last example of the description)
     If no letters are given, the function should return 'z'
