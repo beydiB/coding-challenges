884d4ddccd50802e9e844b63b4c6033a221173c7
beydiB <98928117+beydiB@users.noreply.github.com>
2023-08-03 22:58:10 +0000

    Update CharCodeCalculation.js

--- CharCodeCalculation.js
+++ CharCodeCalculation.js
@@ -2,7 +2,6 @@
 Given a string, turn each character into its ASCII character code and join them together to create a number - let's call this number total1:
 
 'ABC' --> 'A' = 65, 'B' = 66, 'C' = 67 --> 656667
-
 Then replace any incidence of the number 7 with the number 1, and call this number 'total2':
 
 total1 = 656667
