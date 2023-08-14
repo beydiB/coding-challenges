4c02079465b51230867a8b0c7879656ffb4ca2e8
beydiB <98928117+beydiB@users.noreply.github.com>
2023-08-14 23:43:44 +0100

    Update centuryYear.js

--- centuryYear.js
+++ centuryYear.js
@@ -3,7 +3,6 @@ Introduction
 
 The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
 Task
-
 Given a year, return the century it is in.
 Examples
 
