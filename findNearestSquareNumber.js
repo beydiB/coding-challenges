929aeb740fa8dff555c8d435f54971eb77e677fe
beydiB <98928117+beydiB@users.noreply.github.com>
2023-08-17 21:48:49 +0100

    Update findNearestSquareNumber.js

--- findNearestSquareNumber.js
+++ findNearestSquareNumber.js
@@ -4,7 +4,6 @@ Your task is to find the nearest square number, nearest_sq(n) or nearestSq(n), o
 For example, if n = 111, then nearest\_sq(n) (nearestSq(n)) equals 121, since 111 is closer to 121, the square of 11, than 100, the square of 10.
 
 If the n is already the perfect square (e.g. n = 144, n = 81, etc.), you need to just return n.
-
 Good luck :)
 
 Check my other katas:
