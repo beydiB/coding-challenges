7bd0f2d18439408f9d9942a850c9bcfb7c97203f
beydiB <98928117+beydiB@users.noreply.github.com>
2023-08-19 23:50:01 +0100

    Update didSheSayHello.js

--- didSheSayHello.js
+++ didSheSayHello.js
@@ -2,7 +2,6 @@
 You received a whatsup message from an unknown number. Could it be from that girl/boy with a foreign accent you met yesterday evening?
 
 Write a simple function to check if the string contains the word hallo in different languages.
-
 These are the languages of the possible people you met the night before:
 
 hello - english
