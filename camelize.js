b373d74fc4b2042a301cd563cd73cb51b2ba12d1
beydiB <98928117+beydiB@users.noreply.github.com>
2023-08-28 23:59:37 +0100

    Update camelize.js

--- camelize.js
+++ camelize.js
@@ -3,7 +3,6 @@
 Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
 
 That is: removes all dashes, each word after dash becomes uppercased.
-
 Examples:
 
 camelize("background-color") == 'backgroundColor';
