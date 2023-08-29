92ab2fd6c8b47af24a8cfe9df827610e356e609f
beydiB <98928117+beydiB@users.noreply.github.com>
2023-08-29 23:21:16 +0100

    Update disariumNumber.js

--- disariumNumber.js
+++ disariumNumber.js
@@ -1,7 +1,6 @@
 /*
 Definition
 Disarium number is the number that The sum of its digits powered with their respective positions is equal to the number itself.
-
 Task
 Given a number, Find if it is Disarium or not .
 */
