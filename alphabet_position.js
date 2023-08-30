cf20764b69dc9fb8dc0ac17da10f434f9e7a2622
beydiB <98928117+beydiB@users.noreply.github.com>
2023-08-30 23:53:33 +0100

    Update alphabet_position.js

--- alphabet_position.js
+++ alphabet_position.js
@@ -1,6 +1,5 @@
 /*
 Welcome.
-
 In this kata you are required to, given a string, replace every letter with its position in the alphabet.
 
 If anything in the text isn't a letter, ignore it and don't return it.
