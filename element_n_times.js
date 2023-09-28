5c0437bfb80d445bccc1f7a191315390976b9553
beydiB <98928117+beydiB@users.noreply.github.com>
2023-09-28 23:08:51 +0100

    Update element_n_times.js

--- element_n_times.js
+++ element_n_times.js
@@ -1,6 +1,5 @@
 /*
 Enough is enough!
-
 Alice and Bob were on a holiday. Both of them took many pictures of the places they've been, and now they want to show Charlie their entire collection. However, Charlie doesn't like these sessions, since the motif usually repeats. He isn't fond of seeing the Eiffel tower 40 times.
 He tells them that he will only sit for the session if they show the same motif at most N times. Luckily, Alice and Bob are able to encode the motif as a number. Can you help them to remove numbers such that their list contains each number only up to N times, without changing the order?
 Task
